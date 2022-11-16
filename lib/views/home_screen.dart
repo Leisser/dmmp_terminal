59b812c7c779fa773efbdd6ba08d7db4a74ac363
kolynzb <collinsbenda360@gmail.com>
2022-11-16 13:18:12 +0300

    Sccafold Home Screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,6 +13,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(),
+      ),
+    );
   }
 }
