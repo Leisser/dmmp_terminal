d2f47fb642505c54bce34438e5b22390003b4e61
kolynzb <collinsbenda360@gmail.com>
2022-11-16 15:18:27 +0300

    Create Home CTA widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dmmp_terminal/views/signin_screen.dart';
+import 'package:dmmp_terminal/views/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -37,7 +37,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SignInPage(),
+      home: const HomeScreen(),
+      // home: const SignInPage(),
     );
   }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dmmp_terminal/utils/reusables/button.dart';
+import 'package:dmmp_terminal/utils/reusables/reusableText.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -13,10 +15,121 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 35),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                const Color.fromRGBO(248, 221, 7, 0.6).withOpacity(.1),
+                const Color.fromRGBO(237, 30, 36, 0.1).withOpacity(.1),
+              ],
+            )),
+            height: MediaQuery.of(context).size.height * 1,
+            width: MediaQuery.of(context).size.width * 1,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const ReuseText(
+                    text: "Taxi number UBH 827V",
+                    fWeight: FontWeight.bold,
+                    size: 24,
+                  ),
+                  const SizedBox(height: 10),
+                  const ReuseText(
+                    text: "Hi James",
+                    size: 16,
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      HomeCTA(
+                        width: 150,
+                        height: 150,
+                        child: Center(
+                          child: ReuseText(
+                            text: "Start a Trip",
+                            fWeight: FontWeight.bold,
+                            size: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      HomeCTA(
+                        width: 150,
+                        height: 150,
+                        child: Center(
+                          child: ReuseText(
+                            text: "End a Trip",
+                            fWeight: FontWeight.bold,
+                            size: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  const HomeCTA(
+                    width: double.infinity,
+                    height: 150,
+                    child: Text("wagwan"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
+
+class HomeCTA extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final Widget? child;
+
+  const HomeCTA(
+      {super.key,
+      required this.width,
+      required this.height,
+      required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        // padding: const EdgeInsets.all(16.0),
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          // border: const Border(
+          //   top: BorderSide(color: Colors.white, width: 1.0),
+          //   left: BorderSide(color: Colors.white, width: 1.0),
+          // ),
+          color: const Color(0xffF6F2EE),
+          border: Border.all(color: Colors.white, width: 1.0),
+          borderRadius: BorderRadius.circular(14),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(94, 158, 158, 158),
+              blurRadius: 16.0, // soften the shadow
+              spreadRadius: 0.08, //extend the shadow
+              offset: Offset(
+                0.0, // Move to right 5
+                3.0, // Move to bottom 5
+              ),
+            ),
+          ],
+        ),
+        child: child);
   }
 }
