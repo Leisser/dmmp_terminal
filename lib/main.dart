30fc6e3a29d79af7433f97f3c57caa71a9223345
kolynzb <collinsbenda360@gmail.com>
2022-11-16 13:10:23 +0300

    Rename Signin_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'package:dmmp_terminal/utils/reusables/reusableInput.dart';
-import 'package:dmmp_terminal/views/sigin_screen.dart';
+import 'package:dmmp_terminal/views/signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 // import 'dart:convert';
-import 'package:glassmorphism/glassmorphism.dart';
 
-import 'config/constants.dart';
-import 'utils/reusables/button.dart';
-import 'utils/reusables/reusableText.dart';
 // import 'package:kcca_dmmp_mobile/views/home/home.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:http/http.dart' as http;
@@ -40,15 +35,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const SignInPage(),
