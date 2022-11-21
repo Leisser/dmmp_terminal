e5a4100c100dab9ba783c29dc55eb418846b46b0
kolynzb <collinsbenda360@gmail.com>
2022-11-21 19:40:07 +0300

    Complete Passenger trip page

--- lib/views/passenger_trip_screen.dart
+++ lib/views/passenger_trip_screen.dart
@@ -19,160 +19,228 @@ class _PassengerTripScreenState extends State<PassengerTripScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        // appBar: ,
         body: SingleChildScrollView(
           child: Container(
               padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 35),
               decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  const Color.fromRGBO(248, 221, 7, 0.6).withOpacity(.1),
-                  const Color.fromRGBO(237, 30, 36, 0.1).withOpacity(.1),
-                ],
-              )),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    const Color.fromRGBO(248, 221, 7, 0.6).withOpacity(.1),
+                    const Color.fromRGBO(237, 30, 36, 0.1).withOpacity(.1),
+                  ],
+                ),
+              ),
               height: MediaQuery.of(context).size.height * 1,
               width: MediaQuery.of(context).size.width * 1,
               child: Column(
                 children: <Widget>[
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height * .025)
+                        .ceilToDouble(),
+                  ),
+                  Center(
+                    child: ReuseText(
+                      text: "Passengers",
+                      fWeight: FontWeight.bold,
+                      size: (MediaQuery.of(context).size.height * .030)
+                          .ceilToDouble(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height * .025)
+                        .ceilToDouble(),
+                  ),
                   // top
-                  Container(
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            ReuseText(
-                              text: "0123456789",
-                              size:
-                                  (MediaQuery.of(context).size.height * .02041)
-                                      .ceilToDouble(),
-                              fWeight: FontWeight.bold,
-                            ),
-                            ReuseText(
-                              text: "1,280",
-                              size:
-                                  (MediaQuery.of(context).size.height * .02041)
-                                      .ceilToDouble(),
-                              fWeight: FontWeight.bold,
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                            height: (MediaQuery.of(context).size.height * .012)
-                                .ceilToDouble()),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            ReuseText(
-                              text: "Card",
-                              color: const Color(0xff727272),
-                              size: (MediaQuery.of(context).size.height * .015)
-                                  .ceilToDouble(),
-                            ),
-                            ReuseText(
-                              text: "UGX",
-                              color: const Color(0xff727272),
-                              size: (MediaQuery.of(context).size.height * .015)
-                                  .ceilToDouble(),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                            height: (MediaQuery.of(context).size.height * .012)
-                                .ceilToDouble()),
-                        Divider(
-                          color: const Color.fromARGB(255, 69, 69, 69),
-                          height: (MediaQuery.of(context).size.height * .00099)
-                              .toPrecision(1),
-                        ),
-                        SizedBox(
-                            height: (MediaQuery.of(context).size.height * .038)
-                                .toPrecision(1)),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            ReuseText(
-                              text: "DURATION",
-                              color: const Color(0xff727272),
-                              size:
-                                  (MediaQuery.of(context).size.height * .02041)
-                                      .ceilToDouble(),
-                              fWeight: FontWeight.bold,
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                            height: (MediaQuery.of(context).size.height * .012)
-                                .ceilToDouble()),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            ReuseText(
-                              text: "20 minutes",
-                              // color: const Color(0xff727272),
-                              size: (MediaQuery.of(context).size.height * .015)
-                                  .ceilToDouble(),
-                            ),
-                            ReuseText(
-                              text: (MediaQuery.of(context).size.height * .255)
-                                  .ceilToDouble()
-                                  .toString(),
-                              // color: const Color(0xff727272),
-                              size: (MediaQuery.of(context).size.height * .015)
-                                  .ceilToDouble(),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                            height: (MediaQuery.of(context).size.height * .012)
-                                .ceilToDouble()),
-                        Divider(
-                          color: const Color.fromARGB(255, 69, 69, 69),
-                          height: (MediaQuery.of(context).size.height * .00099)
-                              .toPrecision(1),
-                        ),
-                        SizedBox(
-                            height: (MediaQuery.of(context).size.height * .051)
-                                .toPrecision(1)),
-                        HomeCTA(
-                          width: double.infinity,
-                          height: 100,
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                left:
-                                    (MediaQuery.of(context).size.height * .025)
-                                        .ceilToDouble()),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                ReuseText(
-                                  text: "PICKUP",
-                                  color: const Color(0xff727272),
-                                  size: (MediaQuery.of(context).size.height *
-                                          .017)
-                                      .ceilToDouble(),
-                                ),
-                                SizedBox(
-                                    height:
-                                        (MediaQuery.of(context).size.height *
-                                                .012)
-                                            .ceilToDouble()),
-                                ReuseText(
-                                  text: "Wandegeya - Ntinda stage",
-                                  // color: const Color(0xff727272),
-                                  fWeight: FontWeight.bold,
-                                  size: (MediaQuery.of(context).size.height *
-                                          .017)
-                                      .ceilToDouble(),
-                                ),
-                              ],
-                            ),
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          ReuseText(
+                            text: "0123456789",
+                            size: (MediaQuery.of(context).size.height * .02041)
+                                .ceilToDouble(),
+                            fWeight: FontWeight.bold,
+                          ),
+                          ReuseText(
+                            text: "1,280",
+                            size: (MediaQuery.of(context).size.height * .02041)
+                                .ceilToDouble(),
+                            fWeight: FontWeight.bold,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                          height: (MediaQuery.of(context).size.height * .012)
+                              .ceilToDouble()),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          ReuseText(
+                            text: "Card",
+                            color: const Color(0xff727272),
+                            size: (MediaQuery.of(context).size.height * .015)
+                                .ceilToDouble(),
+                          ),
+                          ReuseText(
+                            text: "UGX",
+                            color: const Color(0xff727272),
+                            size: (MediaQuery.of(context).size.height * .015)
+                                .ceilToDouble(),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                          height: (MediaQuery.of(context).size.height * .012)
+                              .ceilToDouble()),
+                      Divider(
+                        color: const Color.fromARGB(255, 69, 69, 69),
+                        height: (MediaQuery.of(context).size.height * .00099)
+                            .toPrecision(1),
+                      ),
+                      SizedBox(
+                          height: (MediaQuery.of(context).size.height * .038)
+                              .toPrecision(1)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          ReuseText(
+                            text: "DURATION",
+                            color: const Color(0xff727272),
+                            size: (MediaQuery.of(context).size.height * .02041)
+                                .ceilToDouble(),
+                            fWeight: FontWeight.bold,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                          height: (MediaQuery.of(context).size.height * .012)
+                              .ceilToDouble()),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          ReuseText(
+                            text: "20 minutes",
+                            // color: const Color(0xff727272),
+                            size: (MediaQuery.of(context).size.height * .015)
+                                .ceilToDouble(),
+                          ),
+                          ReuseText(
+                            text: (MediaQuery.of(context).size.height * .255)
+                                .ceilToDouble()
+                                .toString(),
+                            // color: const Color(0xff727272),
+                            size: (MediaQuery.of(context).size.height * .015)
+                                .ceilToDouble(),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                          height: (MediaQuery.of(context).size.height * .012)
+                              .ceilToDouble()),
+                      Divider(
+                        color: const Color.fromARGB(255, 69, 69, 69),
+                        height: (MediaQuery.of(context).size.height * .00099)
+                            .toPrecision(1),
+                      ),
+                      SizedBox(
+                          height: (MediaQuery.of(context).size.height * .051)
+                              .toPrecision(1)),
+                      HomeCTA(
+                        width: double.infinity,
+                        height: 100,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: (MediaQuery.of(context).size.height * .025)
+                                  .ceilToDouble()),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              ReuseText(
+                                text: "PICKUP",
+                                color: const Color(0xff727272),
+                                size:
+                                    (MediaQuery.of(context).size.height * .017)
+                                        .ceilToDouble(),
+                              ),
+                              SizedBox(
+                                  height: (MediaQuery.of(context).size.height *
+                                          .012)
+                                      .ceilToDouble()),
+                              ReuseText(
+                                text: "Wandegeya - Ntinda stage",
+                                // color: const Color(0xff727272),
+                                fWeight: FontWeight.bold,
+                                size:
+                                    (MediaQuery.of(context).size.height * .017)
+                                        .ceilToDouble(),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(
+                        height: (MediaQuery.of(context).size.height * .025)
+                            .ceilToDouble(),
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color.fromARGB(65, 255, 0, 0),
+                              Color.fromARGB(189, 254, 0, 8),
+                            ],
+                          ),
+                        ),
+                        height: (MediaQuery.of(context).size.height * .14)
+                            .ceilToDouble(),
+                        width: 2,
+                      ),
+                      SizedBox(
+                        height: (MediaQuery.of(context).size.height * .025)
+                            .ceilToDouble(),
+                      ),
+                      HomeCTA(
+                        width: double.infinity,
+                        height: 100,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: (MediaQuery.of(context).size.height * .025)
+                                  .ceilToDouble()),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              ReuseText(
+                                text: "DROP OFF",
+                                color: const Color(0xff727272),
+                                size:
+                                    (MediaQuery.of(context).size.height * .017)
+                                        .ceilToDouble(),
+                              ),
+                              SizedBox(
+                                  height: (MediaQuery.of(context).size.height *
+                                          .012)
+                                      .ceilToDouble()),
+                              ReuseText(
+                                text: "Still on board",
+                                // color: const Color(0xff727272),
+                                fWeight: FontWeight.bold,
+                                size:
+                                    (MediaQuery.of(context).size.height * .017)
+                                        .ceilToDouble(),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               )),
